f7a42b53d80145e1917a346d91af17acc768bc50
budi641 <act.acttract78@gmail.com>
2023-10-14 16:33:08 +0300

    comments but saved this time

--- index.js
+++ index.js
@@ -1,115 +1,153 @@
-const puppeteer = require('puppeteer'); // v20.7.4 or later
+// Import the Puppeteer library for web scraping.
+const puppeteer = require('puppeteer');
+// Import a function called createCalendar from scrap.js.
 const { createCalendar } = require("./scrap");
+// Import the readline module for user input
 const readline = require('readline');
+
+// Initialize variables to store the username and password.
 let username = "";
 let password = "";
+
+// Create a readline interface for user input.
 let rl = readline.createInterface(
     process.stdin, process.stdout);
+
+// Ask the user to input their Self-service username.
 rl.question("Enter your Self-service username: ", (user) => {
-    username = user
+    username = user;
+    // Ask the user to input their Self-service password.
     rl.question("Enter your Self-service password: ", async (pass) => {
-        password = pass
-        console.log("Please wait ...")
-        await createScrapCal()
+        password = pass;
+        console.log("Please wait ...");
+        // Call the createScrapCal function after getting user input.
+        await createScrapCal();
     });
-    
 });
-
-
-
+// Function to perform web scraping and create a calendar.
 const createScrapCal = async () => {
+    // Launch a headless browser instance.
     const browser = await puppeteer.launch({ headless: "new" });
+    // Create a new page in the browser.
     const page = await browser.newPage();
-    const timeout = 60000;
+    const timeout = 60000; // Set a timeout for page actions.
     page.setDefaultTimeout(timeout);
 
+    // Listen for responses from the page.
     page.on("response", (res) => {
+        // Check if the URL matches the Schedule page.
         if (res.url() === `https://sisselfservice.zewailcity.edu.eg/PowerCampusSelfService/Schedule/Student`) {
             res.json().then((res) => {
-                createCalendar(JSON.parse(res))
-                browser.close().then(()=>{
-                    console.log("File saved as calendar.ics")
-                    rl.close()  
+                // Call the createCalendar function with the response data.
+                createCalendar(JSON.parse(res));
+                // Close the browser and provide a message when done.
+                browser.close().then(() => {
+                    console.log("File saved as calendar.ics");
+                    rl.close();
                 });
-                
-            })
+            });
         }
-    })
-    {
-        const targetPage = page;
-        const promises = [];
-        const startWaitingForEvents = () => {
-            promises.push(targetPage.waitForNavigation());
-        }
-        startWaitingForEvents();
-        await targetPage.goto('https://sisselfservice.zewailcity.edu.eg/PowerCampusSelfService/Home/LogIn?ReturnUrl=%2FPowerCampusSelfService%2FRegistration%2FSchedule');
-        await Promise.all(promises);
-    }
+    });
 
-    {
-        const targetPage = page;
-        await puppeteer.Locator.race([
-            targetPage.locator('::-p-aria(Username)'),
-            targetPage.locator('#txtUserName'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"txtUserName\\"])'),
-            targetPage.locator(':scope >>> #txtUserName')
-        ])
-            .setTimeout(timeout)
-            .fill(username);
-    }
+}
 
-    {
-        const targetPage = page;
-        await puppeteer.Locator.race([
-            targetPage.locator('span > span'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"btnNext\\"]/span/span)'),
-            targetPage.locator(':scope >>> span > span'),
-            targetPage.locator('::-p-text(Next)')
-        ])
-            .setTimeout(timeout)
-            .click();
+{
+    const targetPage = page;
+    const promises = [];
+    
+    // Define a function to start waiting for navigation events.
+    const startWaitingForEvents = () => {
+        promises.push(targetPage.waitForNavigation());
     }
+    
+    // Start waiting for events.
+    startWaitingForEvents();
+    
+    // Navigate to the login page.
+    await targetPage.goto('https://sisselfservice.zewailcity.edu.eg/PowerCampusSelfService/Home/LogIn?ReturnUrl=%2FPowerCampusSelfService%2FRegistration%2FSchedule');
+    
+    // Wait for all the promises to resolve.
+    await Promise.all(promises);
+}
 
-    {
-        const targetPage = page;
-        await puppeteer.Locator.race([
-            targetPage.locator('::-p-aria(Password)'),
-            targetPage.locator('#txtPassword'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"txtPassword\\"])'),
-            targetPage.locator(':scope >>> #txtPassword')
-        ])
-            .setTimeout(timeout)
-            .fill(password);
-    }
+{
+    const targetPage = page;
+    
+    // Locate the username input field using different selectors.
+    await puppeteer.Locator.race([
+        targetPage.locator('::-p-aria(Username)'),
+        targetPage.locator('#txtUserName'),
+        targetPage.locator('::-p-xpath(//*[@id=\\"txtUserName\\"])'),
+        targetPage.locator(':scope >>> #txtUserName')
+    ])
+    .setTimeout(timeout)
+    .fill(username); // Fill the username field with the provided username.
+}
 
-    {
-        const targetPage = page;
-        const promises = [];
-        const startWaitingForEvents = () => {
-            promises.push(targetPage.waitForNavigation());
-        }
-        await puppeteer.Locator.race([
-            targetPage.locator('div.jssEGb7Iu2BR_422 span > span'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"btnSignIn\\"]/span/span)'),
-            targetPage.locator(':scope >>> div.jssEGb7Iu2BR_422 span > span'),
-            targetPage.locator('::-p-text(Sign in)')
-        ])
-            .on('action', () => startWaitingForEvents())
-            .click();
-        await Promise.all(promises);
-    }
+{
+    const targetPage = page;
+    
+    // Locate and click the "Next" button.
+    await puppeteer.Locator.race([
+        targetPage.locator('span > span'),
+        targetPage.locator('::-p-xpath(//*[@id=\\"btnNext\\"]/span/span)'),
+        targetPage.locator(':scope >>> span > span'),
+        targetPage.locator('::-p-text(Next)')
+    ])
+    .setTimeout(timeout)
+    .click(); // Click the "Next" button.
+}
 
-    {
-        const targetPage = page;
-        await puppeteer.Locator.race([
-            targetPage.locator('::-p-aria(Period 2022/FALL)'),
-            targetPage.locator('#ddlPeriod_value'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"ddlPeriod_value\\"])'),
-            targetPage.locator(':scope >>> #ddlPeriod_value')
-        ])
-            .setTimeout(timeout)
-            .click();
+{
+    const targetPage = page;
+    
+    // Locate the password input field using different selectors.
+    await puppeteer.Locator.race([
+        targetPage.locator('::-p-aria(Password)'),
+        targetPage.locator('#txtPassword'),
+        targetPage.locator('::-p-xpath(//*[@id=\\"txtPassword\\"])'),
+        targetPage.locator(':scope >>> #txtPassword')
+    ])
+    .setTimeout(timeout)
+    .fill(password); // Fill the password field with the provided password.
+}
+
+{
+    const targetPage = page;
+    const promises = [];
+    
+    // Define a function to start waiting for navigation events.
+    const startWaitingForEvents = () => {
+        promises.push(targetPage.waitForNavigation());
     }
+    
+    // Locate and click the "Sign in" button, while waiting for navigation.
+    await puppeteer.Locator.race([
+        targetPage.locator('div.jssEGb7Iu2BR_422 span > span'),
+        targetPage.locator('::-p-xpath(//*[@id=\\"btnSignIn\\"]/span/span)'),
+        targetPage.locator(':scope >>> div.jssEGb7Iu2BR_422 span > span'),
+        targetPage.locator('::-p-text(Sign in)')
+    ])
+    .on('action', () => startWaitingForEvents())
+    .click(); // Click the "Sign in" button.
+    
+    // Wait for all the promises to resolve.
+    await Promise.all(promises);
+}
+
+{
+    const targetPage = page;
+    
+    // Locate and click the "Period 2022/FALL" dropdown.
+    await puppeteer.Locator.race([
+        targetPage.locator('::-p-aria(Period 2022/FALL)'),
+        targetPage.locator('#ddlPeriod_value'),
+        targetPage.locator('::-p-xpath(//*[@id=\\"ddlPeriod_value\\"])'),
+        targetPage.locator(':scope >>> #ddlPeriod_value')
+    ])
+    .setTimeout(timeout)
+    .click(); // Click the dropdown to select the period.
+}
 
     /*  {
          const targetPage = page;
@@ -118,4 +156,3 @@ const createScrapCal = async () => {
          console.log(semesters)
      } */
 
-}
--- scrap.js
+++ scrap.js
@@ -1,42 +1,62 @@
-const { datetime, RRule, RRuleSet, rrulestr } = require('rrule')
-const ics = require('ics')
-const fs = require("fs")
+// Import necessary libraries and modules.
+const { datetime, RRule, RRuleSet, rrulestr } = require('rrule');
+const ics = require('ics');
+const fs = require("fs");
 
+// Define a function to create a calendar from the provided JSON data.
 function createCalendar(ssJson) {
-    // 2 nested loops first loops on subject second loops in schedules where the actual logic lies\
-    const YEARI = 2,
-        MONTHI = 0,
-        DAYI = 1
-    let events = []
+    // Constants for date indices.
+    const YEARI = 2;
+    const MONTHI = 0;
+    const DAYI = 1;
+
+    // Initialize an empty array to store calendar events.
+    let events = [];
+
+    // Loop through subjects in the JSON data.
     for (let i = 0; i < ssJson.data["schedule"][0].sections[3].length; i++) {
-        let subj = ssJson.data.schedule[0].sections[3][i]
-        subj.startDate = subj.startDate.split("/").map(el => el * 1)
-        subj.endDate = subj.endDate.split("/").map(el => el * 1)
+        let subj = ssJson.data.schedule[0].sections[3][i];
+
+        // Convert date strings to arrays.
+        subj.startDate = subj.startDate.split("/").map(el => el * 1);
+        subj.endDate = subj.endDate.split("/").map(el => el * 1);
+
+        // Loop through lecture schedules within the subject.
         for (let lectIndex = 0; lectIndex < subj.schedules.length; lectIndex++) {
-            let event = {}
-            let day = new Date(parseInt(subj.startDate[2]), parseInt(subj.startDate[0]) - 1, parseInt(subj.startDate[1]))
+            let event = {};
+            let day = new Date(
+                parseInt(subj.startDate[2]),
+                parseInt(subj.startDate[0]) - 1,
+                parseInt(subj.startDate[1])
+            );
+
+            // Adjust the start date based on scheduled days.
             if (day.getDay() < subj.schedules[lectIndex]["scheduledDays"][0]) {
-                subj.startDate[1] += subj.schedules[lectIndex]["scheduledDays"][0] - day.getDay()
-                // console.log(day.getDay())
+                subj.startDate[1] += subj.schedules[lectIndex]["scheduledDays"][0] - day.getDay();
             }
             if (day.getDay() > subj.schedules[lectIndex]["scheduledDays"][0]) {
-                subj.startDate[1] -= day.getDay() - subj.schedules[lectIndex]["scheduledDays"][0]
-                // console.log(day.getDay(), "ff")
+                subj.startDate[1] -= day.getDay() - subj.schedules[lectIndex]["scheduledDays"][0];
             }
 
-            event.title = `${subj.eventId} - ${subj.eventName.replaceAll("\n", "")} (${subj.eventSubType})`
-            event.start = [subj.startDate[YEARI],
-            subj.startDate[MONTHI],
-            subj.startDate[DAYI],
-            subj.schedules[lectIndex].scheduledStartTime[0],
-            subj.schedules[lectIndex].scheduledStartTime[1]]
-            event.startOutputType = "local"
-            event.end = [subj.startDate[YEARI],
-            subj.startDate[MONTHI],
-            subj.startDate[DAYI],
-            subj.schedules[lectIndex].scheduledEndTime[0],
-            subj.schedules[lectIndex].scheduledEndTime[1]]
+            // Configure event properties.
+            event.title = `${subj.eventId} - ${subj.eventName.replaceAll("\n", "")} (${subj.eventSubType})`;
+            event.start = [
+                subj.startDate[YEARI],
+                subj.startDate[MONTHI],
+                subj.startDate[DAYI],
+                subj.schedules[lectIndex].scheduledStartTime[0],
+                subj.schedules[lectIndex].scheduledStartTime[1]
+            ];
+            event.startOutputType = "local";
+            event.end = [
+                subj.startDate[YEARI],
+                subj.startDate[MONTHI],
+                subj.startDate[DAYI],
+                subj.schedules[lectIndex].scheduledEndTime[0],
+                subj.schedules[lectIndex].scheduledEndTime[1]
+            ];
 
+            // Define a recurrence rule for weekly events.
             event.recurrenceRule = new RRule({
                 freq: RRule.WEEKLY,
                 until: new Date(
@@ -46,18 +66,22 @@ function createCalendar(ssJson) {
                     subj.schedules[lectIndex].scheduledEndTime[0],
                     subj.schedules[lectIndex].scheduledEndTime[1]
                 )
-            }).toString().replace("RRULE:", "")
-            event.location = `${subj.schedules[lectIndex].orgName}, ${subj.schedules[lectIndex].bldgName},  ${subj.schedules[lectIndex].roomId}`
-            events.push(event)
-        }
+            }).toString().replace("RRULE:", "");
 
+            event.location = `${subj.schedules[lectIndex].orgName}, ${subj.schedules[lectIndex].bldgName},  ${subj.schedules[lectIndex].roomId}`;
+
+            // Add the event to the array.
+            events.push(event);
+        }
     }
 
+    // Create an iCalendar (ICS) file from the events and save it.
     ics.createEvents(events, (err, val) => {
+        fs.writeFileSync("calendar.ics", val);
+    });
 
-        fs.writeFileSync("calendar.ics",val)
-    })
-    // return schedule.data.schedule[0].sections[3][0]
+    // Export the function to make it available for use in other modules.
 }
 
-module.exports = {createCalendar}
+// Export the function as a module.
+module.exports = { createCalendar };
