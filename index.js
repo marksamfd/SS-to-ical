548747c83a33ca767351352eb497a5c9c96d9afa
mark Samuel <marksamfd511@gmail.com>
2023-10-28 16:04:27 +0200

    fixed problem with async scoping

--- index.js
+++ index.js
@@ -49,110 +49,102 @@ const createScrapCal = async () => {
         }
     });
 
-}
+    {
+        const targetPage = page;
+        const promises = [];
 
-{
-    const targetPage = page;
-    const promises = [];
-    
-    // Define a function to start waiting for navigation events.
-    const startWaitingForEvents = () => {
-        promises.push(targetPage.waitForNavigation());
+        // Define a function to start waiting for navigation events.
+        const startWaitingForEvents = () => {
+            promises.push(targetPage.waitForNavigation());
+        }
+
+        // Start waiting for events.
+        startWaitingForEvents();
+
+        // Navigate to the login page.
+        await targetPage.goto('https://sisselfservice.zewailcity.edu.eg/PowerCampusSelfService/Home/LogIn?ReturnUrl=%2FPowerCampusSelfService%2FRegistration%2FSchedule');
+
+        // Wait for all the promises to resolve.
+        await Promise.all(promises);
     }
-    
-    // Start waiting for events.
-    startWaitingForEvents();
-    
-    // Navigate to the login page.
-    await targetPage.goto('https://sisselfservice.zewailcity.edu.eg/PowerCampusSelfService/Home/LogIn?ReturnUrl=%2FPowerCampusSelfService%2FRegistration%2FSchedule');
-    
-    // Wait for all the promises to resolve.
-    await Promise.all(promises);
-}
 
-{
-    const targetPage = page;
-    
-    // Locate the username input field using different selectors.
-    await puppeteer.Locator.race([
-        targetPage.locator('::-p-aria(Username)'),
-        targetPage.locator('#txtUserName'),
-        targetPage.locator('::-p-xpath(//*[@id=\\"txtUserName\\"])'),
-        targetPage.locator(':scope >>> #txtUserName')
-    ])
-    .setTimeout(timeout)
-    .fill(username); // Fill the username field with the provided username.
-}
+    {
+        const targetPage = page;
 
-{
-    const targetPage = page;
-    
-    // Locate and click the "Next" button.
-    await puppeteer.Locator.race([
-        targetPage.locator('span > span'),
-        targetPage.locator('::-p-xpath(//*[@id=\\"btnNext\\"]/span/span)'),
-        targetPage.locator(':scope >>> span > span'),
-        targetPage.locator('::-p-text(Next)')
-    ])
-    .setTimeout(timeout)
-    .click(); // Click the "Next" button.
-}
-
-{
-    const targetPage = page;
-    
-    // Locate the password input field using different selectors.
-    await puppeteer.Locator.race([
-        targetPage.locator('::-p-aria(Password)'),
-        targetPage.locator('#txtPassword'),
-        targetPage.locator('::-p-xpath(//*[@id=\\"txtPassword\\"])'),
-        targetPage.locator(':scope >>> #txtPassword')
-    ])
-    .setTimeout(timeout)
-    .fill(password); // Fill the password field with the provided password.
-}
-
-{
-    const targetPage = page;
-    const promises = [];
-    
-    // Define a function to start waiting for navigation events.
-    const startWaitingForEvents = () => {
-        promises.push(targetPage.waitForNavigation());
+        // Locate the username input field using different selectors.
+        await puppeteer.Locator.race([
+            targetPage.locator('::-p-aria(Username)'),
+            targetPage.locator('#txtUserName'),
+            targetPage.locator('::-p-xpath(//*[@id=\\"txtUserName\\"])'),
+            targetPage.locator(':scope >>> #txtUserName')
+        ])
+            .setTimeout(timeout)
+            .fill(username); // Fill the username field with the provided username.
     }
-    
-    // Locate and click the "Sign in" button, while waiting for navigation.
-    await puppeteer.Locator.race([
-        targetPage.locator('div.jssEGb7Iu2BR_422 span > span'),
-        targetPage.locator('::-p-xpath(//*[@id=\\"btnSignIn\\"]/span/span)'),
-        targetPage.locator(':scope >>> div.jssEGb7Iu2BR_422 span > span'),
-        targetPage.locator('::-p-text(Sign in)')
-    ])
-    .on('action', () => startWaitingForEvents())
-    .click(); // Click the "Sign in" button.
-    
-    // Wait for all the promises to resolve.
-    await Promise.all(promises);
-}
 
-{
-    const targetPage = page;
-    
-    // Locate and click the "Period 2022/FALL" dropdown.
-    await puppeteer.Locator.race([
-        targetPage.locator('::-p-aria(Period 2022/FALL)'),
-        targetPage.locator('#ddlPeriod_value'),
-        targetPage.locator('::-p-xpath(//*[@id=\\"ddlPeriod_value\\"])'),
-        targetPage.locator(':scope >>> #ddlPeriod_value')
-    ])
-    .setTimeout(timeout)
-    .click(); // Click the dropdown to select the period.
-}
+    {
+        const targetPage = page;
 
-    /*  {
-         const targetPage = page;
- 
-         let semesters = await targetPage.$$('#menu- > div.MuiPaper-root-0r_c4u2pkR450.MuiMenu-paper-0r_c4u2pkR960.Dropdown-paper-0r_c4u2pkR876.MuiPopover-paper-0r_c4u2pkR963.MuiPaper-elevation8-0r_c4u2pkR461.MuiPaper-rounded-0r_c4u2pkR451 > ul > li')
-         console.log(semesters)
-     } */
+        // Locate and click the "Next" button.
+        await puppeteer.Locator.race([
+            targetPage.locator('span > span'),
+            targetPage.locator('::-p-xpath(//*[@id=\\"btnNext\\"]/span/span)'),
+            targetPage.locator(':scope >>> span > span'),
+            targetPage.locator('::-p-text(Next)')
+        ])
+            .setTimeout(timeout)
+            .click(); // Click the "Next" button.
+    }
+
+    {
+        const targetPage = page;
+
+        // Locate the password input field using different selectors.
+        await puppeteer.Locator.race([
+            targetPage.locator('::-p-aria(Password)'),
+            targetPage.locator('#txtPassword'),
+            targetPage.locator('::-p-xpath(//*[@id=\\"txtPassword\\"])'),
+            targetPage.locator(':scope >>> #txtPassword')
+        ])
+            .setTimeout(timeout)
+            .fill(password); // Fill the password field with the provided password.
+    }
+
+    {
+        const targetPage = page;
+        const promises = [];
+
+        // Define a function to start waiting for navigation events.
+        const startWaitingForEvents = () => {
+            promises.push(targetPage.waitForNavigation());
+        }
+
+        // Locate and click the "Sign in" button, while waiting for navigation.
+        await puppeteer.Locator.race([
+            targetPage.locator('div.jssEGb7Iu2BR_422 span > span'),
+            targetPage.locator('::-p-xpath(//*[@id=\\"btnSignIn\\"]/span/span)'),
+            targetPage.locator(':scope >>> div.jssEGb7Iu2BR_422 span > span'),
+            targetPage.locator('::-p-text(Sign in)')
+        ])
+            .on('action', () => startWaitingForEvents())
+            .click(); // Click the "Sign in" button.
+
+        // Wait for all the promises to resolve.
+        await Promise.all(promises);
+    }
+
+    {
+        const targetPage = page;
+
+        // Locate and click the "Period 2022/FALL" dropdown.
+        await puppeteer.Locator.race([
+            targetPage.locator('::-p-aria(Period 2022/FALL)'),
+            targetPage.locator('#ddlPeriod_value'),
+            targetPage.locator('::-p-xpath(//*[@id=\\"ddlPeriod_value\\"])'),
+            targetPage.locator(':scope >>> #ddlPeriod_value')
+        ])
+            .setTimeout(timeout)
+            .click(); // Click the dropdown to select the period.
+    }
+}
 
